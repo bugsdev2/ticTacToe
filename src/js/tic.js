5a382e34e9844c353208856e926661088b88bb05
BugsLaptop <msherlock246@gmail.com>
2023-09-29 16:24:41 +0530

    Basic functionality is done. Got the game to work in a two player mode. But still have to work on the strikethrough.

--- src/js/tic.js
+++ src/js/tic.js
@@ -7,11 +7,37 @@ export const tic = (function() {
 		for(let i=0; i<row; i++) {
 			board[i] = [];
 			for(let j=0; j<col; j++) {
-				board[i][j] = ' ';
+				board[i][j] = null;
 			};
 		}
 		return board;
 	}
+	
+	const displayController = (function() {
+		const board = GameBoard();
+		const boardElem = document.querySelector('.board');
+		board.forEach((row, rowIndex) => {
+			row.forEach((tile, tileIndex) => {
+				const tileElem = document.createElement('div');
+				tileElem.dataset.tile = 'Row-' + (rowIndex+1) + ' Tile-' + (tileIndex+1);
+				boardElem.appendChild(tileElem);
+			});
+			
+			// board based on rows and columns (having display issues)
+			
+			//~ const rowElem = document.createElement('div');
+			//~ rowElem.dataset.row = 'Row-' + (index+1);
+			//~ row.forEach((tile,index) => {
+				//~ const tileElem = document.createElement('span');
+				//~ tileElem.dataset.tile = 'Tile-' + (index+1);
+				//~ rowElem.appendChild(tileElem);
+			//~ });
+			//~ boardElem.appendChild(rowElem);
+			
+		});
+		
+		
+	})();
 	
 	const GameController = (function(playerOne = 'Player 1', playerTwo = 'Player 2') {
 		const players = [
@@ -23,7 +49,6 @@ export const tic = (function() {
 			name: playerTwo,
 			token: 'O'
 		}
-		
 		]
 		
 		let activePlayer = players[0];
@@ -34,42 +59,82 @@ export const tic = (function() {
 			return activePlayer;
 		}
 		const boardElem = document.querySelector('.board');
-		const currentPlayerElem = document.querySelector('.current-player');
-		currentPlayerElem.textContent = players[0].name;
-		boardElem.addEventListener('click', updateTile);
-		boardElem.addEventListener('click', updateNameBoard);
+		const messageBoard = document.querySelector('.message-board');
+		messageBoard.textContent = players[0].name;
 		
-		function updateNameBoard(e){
-			currentPlayerElem.textContent = activePlayer.name;
+		boardElem.addEventListener('click', updateNameBoard(activePlayer.name));
+		
+		function updateNameBoard(message){
+			messageBoard.textContent = message;
 		};
 		
 		function updateTile(e) {
-		if (e.target.textContent == ' ') {
-			e.target.textContent = activePlayer.token;
-			activePlayer = switchPlayer();
-		} else {
-			return;
-		}
-	};
+			if (e.target.textContent == '') {
+				e.target.textContent = activePlayer.token;
+				let winner = checkWinner();
+				if (winner === undefined) {
+					activePlayer = switchPlayer();
+					updateNameBoard(activePlayer.name);
+				} else {
+					let won = winner + ' is the Winner'
+					updateNameBoard(won);
+					endGame();
+					button.textContent = 'Start New Game';
+					button.classList.remove('btn-warning');
+					button.classList.add('btn-success');
+				}
+			} else {
+				return;
+			}
+		};
 		
-		return { switchPlayer }
+		//function to check for winner
+		function checkWinner() {
+			const tiles = document.querySelectorAll('[data-tile]');
+			if(tiles[0].textContent === activePlayer.token && tiles[1].textContent === activePlayer.token && tiles[2].textContent === activePlayer.token ||
+				tiles[3].textContent === activePlayer.token && tiles[4].textContent === activePlayer.token && tiles[5].textContent === activePlayer.token ||
+				tiles[6].textContent === activePlayer.token && tiles[7].textContent === activePlayer.token && tiles[8].textContent === activePlayer.token ||
+				tiles[0].textContent === activePlayer.token && tiles[4].textContent === activePlayer.token && tiles[8].textContent === activePlayer.token ||
+				tiles[2].textContent === activePlayer.token && tiles[4].textContent === activePlayer.token && tiles[6].textContent === activePlayer.token ||
+				tiles[0].textContent === activePlayer.token && tiles[3].textContent === activePlayer.token && tiles[6].textContent === activePlayer.token ||
+				tiles[1].textContent === activePlayer.token && tiles[4].textContent === activePlayer.token && tiles[7].textContent === activePlayer.token ||
+				tiles[3].textContent === activePlayer.token && tiles[5].textContent === activePlayer.token && tiles[8].textContent === activePlayer.token) {
+					return activePlayer.name;
+			}
+		}
+		
+		// function to start Game
+		function startGame(){
+			if(button.textContent == 'Game Started') return;
+			clearBoard();
+			button.textContent = 'Game Started';
+			button.classList.remove('btn-success');
+			button.classList.add('btn-warning');
+			boardElem.addEventListener('click', updateNameBoard(activePlayer.name));
+			boardElem.addEventListener('click', updateTile);
+			
+		};
+		
+		// function to end Game
+		function endGame(){
+			
+			boardElem.removeEventListener('click', updateTile);
+			boardElem.removeEventListener('click', updateNameBoard);
+		};
+		
+		function clearBoard() {
+			const tiles = document.querySelectorAll('[data-tile]');
+			tiles.forEach(tile => {
+				tile.textContent = null;
+			});
+		};
+		
+		return { startGame }
 		
 	})();
 	
-	const displayController = (function() {
-		const board = GameBoard();
-		const boardElem = document.querySelector('.board');
-		board.forEach(row => {
-			row.forEach(tile => {
-				const tileElem = document.createElement('div');
-				tileElem.textContent = tile;
-				tileElem.dataset.tile= tile;
-				boardElem.appendChild(tileElem);
-			});
-		});
-		
-		
-	})();
+	const button = document.querySelector('button');
+	button.addEventListener('click', GameController.startGame);
 	
 	
 })();
